71175524d53cd97c951544983b74a62019a205dc
SamiranKumar <samirankumar11@gmail.com>
2018-11-20 16:44:07 +0600

    create JNI package/folder

---
+++ OpenCVAndroid/app/src/main/java/com/skh/shomu/open_cv_android/ReadMe.java
@@ -0,0 +1,33 @@
+package com.skh.shomu.open_cv_android;
+
+public class ReadMe {
+
+    /*
+
+    Setup OpenCV sdk in Android Stdio
+
+    1. Download Open-CV sdk and Extract download zip file
+        *URL : https://sourceforge.net/projects/opencvlibrary/
+        *Ref : https://opencv.org/
+
+
+    2.Create android stdio project
+
+    3. Import Open-CV sdk in project
+        *  File > New > Import Module..
+          > if problem sync snd version then remove sdk version in manifests.xml file
+          > add defaultConfig in gradle file Open-CV module
+          > add module dependencies in project gradle file
+          > implementation project(':openCVLibrary320')
+
+
+
+
+
+
+
+
+     */
+
+
+}
